b5f869d5364af7403f07103de7acb5f3b5826e07
elfred434 <elfred434@gmail.com>
2026-09-20 13:55:32 +0100

    first commit

---
+++ taskmaster/taskmater.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 13:52:31 2026
+
+@author: elfre
+"""
+app_name= "Taskmaster"
+auteur = "Elfred Dangbenon"
+version = 0.1
---
+++ variables.py
@@ -0,0 +1,11 @@
+Nom = "Elfred"
+Ville = "PortoNovo"
+
+a = 17
+b = 5
+print("Mon nom est ", Nom, "Je vis à ", Ville)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
