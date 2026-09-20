b4b4a5bae706c7d370def0e372af1f2df9cb5eb9
elfred434 <elfred434@gmail.com>
2026-09-20 14:08:14 +0100

    Mission S1

--- taskmaster/taskmater.py
+++ taskmaster/taskmater.py
@@ -7,3 +7,8 @@ Created on Sun Sep 20 13:52:31 2026
 app_name= "Taskmaster"
 auteur = "Elfred Dangbenon"
 version = 0.1
+
+print("======================")
+print("  ",app_name,"v"+str(version))
+print("  ","par",auteur )
+print("======================")
--- variables.py
+++ variables.py
@@ -3,7 +3,7 @@ Ville = "PortoNovo"
 
 a = 17
 b = 5
-print("Mon nom est ", Nom, "Je vis à ", Ville)
+print("Mon nom est", Nom, "Je vis à", Ville)
 print(a+b)
 print(a-b)
 print(a*b)
